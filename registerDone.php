2cd99793b4780c2fad58d7b3f6a209b3f93256e8
Sanjana_Nalajitha <snalajitha@gmail.com>
2025-04-24 15:43:16 +0530

    registerDone php file added

---
+++ registerDone.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = $_POST['uname'];
+$email = $_POST['email'];
+$password = $_POST['pword'];
+$conPassword = $_POST['rePword'];
+
+echo $username;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+?>
